ec7b6983514a6fc82d31bb8a9105e8c0905de5b9
ArizCefDev <arizcefdev@gmail.com>
2023-09-27 22:40:56 +0400

    update

--- WebShop/Controllers/AdminController.cs
+++ WebShop/Controllers/AdminController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebShop.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        //Product
+        public IActionResult ProductIndex()
+        {
+            var values = _productService.GetAll();
+            return View(values.OrderByDescending(x=>x.ID));
         }
     }
 }
--- WebShop/Controllers/HomeController.cs
+++ WebShop/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProductService _productService;
+
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var values = _productService.GetAll();
+            return View(values.OrderByDescending(x=>x.ID));
         }
     }
 }
